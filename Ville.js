f23fcdab1af9c81e3d54d7457dd5506d27845c0e
Mael <mael.thomas@data.gouv.fr>
2019-12-18 17:34:45 +0100

    On stocke la requète /:ville dans le navigateur
    On peut maintenant étudier ces données sans faire trop de requêtes
    inutiles

--- Ville.js
+++ Ville.js
@@ -1,3 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { compute } from './allez'
+import localforage from 'localforage'
 
-export default ({ match: { params } }) => <h1>{params.ville}</h1>
+export default ({ match: { params } }) => {
+	let ville = params.ville
+	let [data, setData] = useState(null)
+	useEffect(() => {
+		localforage.getItem(ville).then(value => {
+			if (!value) {
+				compute(ville, data => {
+					localforage.setItem(ville, data)
+					setData(data)
+				})
+			}
+			setData(value)
+		})
+	}, [])
+
+	return (
+		<div>
+			<h1>{params.ville}</h1>
+			<pre>
+				{data &&
+					JSON.stringify(
+						{ ...data, geojson: data.geojson.features.length },
+						null,
+						'\t'
+					)}
+			</pre>
+			Bientôt une carte
+			<button onClick={() => localforage.clear()}>Vider la mémoire</button>
+		</div>
+	)
+}
--- allez.js
+++ allez.js
@@ -15,20 +15,23 @@ let score = geoJson => {
 	let areas = geoJson.features
 		.filter(f => f.geometry.type === 'Polygon')
 		.map(f => geojsonArea.geometry(f.geometry))
-	console.log('areas', areas.length)
+	let areasLength = areas.length
 	let totalAreas = areas.reduce((memo, next) => memo + next, 0)
-	console.log('totalAreas', totalAreas / 1000)
 	let lines = geoJson.features
 		.filter(f => f.geometry.type === 'LineString')
 		.map(f => geojsonLength(f.geometry))
-	console.log('lines', lines.length)
+	let linesLength = lines.length
 	let totalLinesLength = lines.reduce((memo, next) => memo + next, 0)
 	let totalLinesArea = totalLinesLength * 5 // we arbitrarily define the average width of a pedestrian street to 5 meters
-	console.log('totalLineAreas', totalLinesArea / 1000)
-	console.log('totaltotal', (totalLinesArea + totalAreas) / 1000)
+	let totalTotal = totalLinesArea + totalAreas
+	return {
+		areasLength,
+		totalAreas,
+		linesLength,
+		totalLinesArea,
+		totalTotal
+	}
 }
-
-let city = process.argv[2]
 
 // Uselfull maybe to disambiguate or
 let findCity = city =>
@@ -36,12 +39,15 @@ let findCity = city =>
 		`https://nominatim.openstreetmap.org/search/${city}?format=json&addressdetails=1&limit=1`
 	).then(r => r.json())
 
-let request = escape(makeRequest(city))
-fetch(`
+export let compute = (city, callback) => {
+	let request = escape(makeRequest(city))
+	fetch(`
 https://www.overpass-api.de/api/interpreter?data=${request}`)
-	.then(r => r.json())
-	.then(json => {
-		let geojson = osmtogeojson(json)
-		console.log(countTypes(geojson))
-		score(geojson)
-	})
+		.then(r => r.json())
+		.then(json => {
+			let geojson = osmtogeojson(json)
+			let typesCount = countTypes(geojson)
+			let cityScore = score(geojson)
+			callback({ geojson, ...cityScore, typesCount })
+		})
+}
