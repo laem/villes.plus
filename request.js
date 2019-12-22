b90b2f76ee00dd2d4fac609b100629e906ed4569
Mael <mael.thomas@data.gouv.fr>
2019-12-22 22:30:31 +0100

    Amélioration de la requête.
    Les relations couvrent des endroits auparavant oubliés. Exemple, la
    place de la bourse à bordeaux.

--- request.js
+++ request.js
@@ -5,29 +5,26 @@ export default placeName => `
 
 
 (
-
-  way["place"="square"](area.searchArea);
-
   way["leisure"="park"](area.searchArea);
+  relation["leisure"="park"](area.searchArea);
   way["leisure"="garden"](area.searchArea);
+  relation["leisure"="garden"](area.searchArea);
 
   way["highway"="footway"]["footway"!~"sidewalk|crossing"](area.searchArea);
 
   way["highway"="pedestrian"]["foot"!="private"](area.searchArea);
+  relation["highway"="pedestrian"]["foot"!="private"](area.searchArea);
 
   way["landuse"="grass"](area.searchArea);
+  relation["landuse"="grass"](area.searchArea);
 
   way["landuse"="recreation_ground"](area.searchArea);
+  relation["landuse"="recreation_ground"](area.searchArea);
 
 );
 
 
-out meta;
-
->;
-
-out meta qt;
-
+out geom;
 
 
 
