cff31922c8fb56d816fed4017dbeb57dc9a2c045
Mael <mael.thomas@data.gouv.fr>
2019-12-26 13:47:55 +0100

    Amélioration de la requête

--- request.js
+++ request.js
@@ -15,11 +15,22 @@ export default placeName => `
   way["highway"="pedestrian"]["foot"!="private"](area.searchArea);
   relation["highway"="pedestrian"]["foot"!="private"](area.searchArea);
 
+  */ 
+    These are excluded, since they do not mean it is a public pedestrian place
+    E.g. the Nice Airport has lots of landuse=grass
+    Also, private residential gardens
+    True positives are usually enclosed in a another area covered by the request, e.g. leisure=park
   way["landuse"="grass"](area.searchArea);
   relation["landuse"="grass"](area.searchArea);
+  */
 
   way["landuse"="recreation_ground"](area.searchArea);
   relation["landuse"="recreation_ground"](area.searchArea);
+
+  /* We've excluded the "squares", since lots of them have cars or parkings. 
+  E.g. name=Place Max Barel in Nice
+  Pedestrian squares should have the highway=pedestrian tag
+  */
 
 );
 
---
+++ requestCityArea.js
@@ -0,0 +1,6 @@
+export default `
+area[name="Bordeaux"][admin_level="8"];
+rel(pivot);
+// print results
+out body geom;
+`
