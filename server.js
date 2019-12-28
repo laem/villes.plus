cf0334af18637fc038cf4ed589df887b2aa9ccad
Mael <mael.thomas@data.gouv.fr>
2019-12-28 13:46:27 +0100

    Compression des requêtes serveur

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { compute } from './allez.js'
 import cors from 'cors'
 import fs from 'fs'
 import path from 'path'
+import compression from 'compression'
 
 const cacheDir = __dirname + '/cache'
 
@@ -14,6 +15,7 @@ if (!fs.existsSync(cacheDir)) {
 
 const app = express()
 app.use(cors())
+app.use(compression())
 
 app.use(express.static(__dirname))
 
