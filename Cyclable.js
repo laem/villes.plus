a48803e9dc373b80354c6d01d716b0f913853749
Mael <mael.thomas@data.gouv.fr>
2022-10-14 11:38:47 +0200

    Stockage d'un trajet et affichage

--- Cyclable.js
+++ Cyclable.js
@@ -1,11 +1,38 @@
-import { Map, Marker } from 'pigeon-maps'
-import { useState } from 'react'
+import { Map, Marker, GeoJson } from 'pigeon-maps'
+import { useEffect, useState } from 'react'
+
+const computeBikeDistance = (from, to) =>
+	fetch(
+		`https://brouter.de/brouter?lonlats=${from.reverse().join(',')}|${to
+			.reverse()
+			.join(',')}&profile=shortest&alternativeidx=0&format=geojson`
+	)
+		.then((res) => res.json())
+		.catch((e) => console.log(e))
+
+const center = [48.10999850495452, -1.679193852233965]
 
 export default () => {
 	const [couple, setCouple] = useState({ from: null, to: null })
 	console.log(couple)
+	const [data, setData] = useState(null) // use an empty array as initial value
+
+	useEffect(() => {
+		if (!couple.to) return undefined
+		computeBikeDistance(couple.from, couple.to).then((res) => {
+			setData(res) // set the state
+		})
+	}, [couple])
+
+	console.log(data)
+
 	return (
-		<div>
+		<div
+			css={`
+				max-width: 1000px;
+				margin: 0 auto;
+			`}
+		>
 			<h1>Ma ville est-elle cyclable ?</h1>
 			<p>
 				Précisons : <em>vraiment</em> cyclable, donc avec des pistes cyclables
@@ -15,17 +42,32 @@ export default () => {
 				Pour le découvrir, cliquez 2 points sur la carte, on vous le dira. Puis
 				recommencez :)
 			</p>
-			{couple.from}
-			{couple.to}
 
 			<Map
-				height={300}
-				defaultCenter={[50.879, 4.6997]}
-				defaultZoom={11}
+				height={'70vh'}
+				width={'70vw'}
+				defaultCenter={center}
+				defaultZoom={13}
 				onClick={({ event, latLng, pixel }) => {
 					setCouple(!couple.from ? { from: latLng } : { ...couple, to: latLng })
 				}}
 			>
+				{data && (
+					<GeoJson
+						data={data}
+						styleCallback={(feature, hover) => {
+							if (feature.geometry.type === 'LineString') {
+								return { strokeWidth: '3', stroke: 'red' }
+							}
+							return {
+								fill: '#d4e6ec99',
+								strokeWidth: '1',
+								stroke: 'white',
+								r: '20',
+							}
+						}}
+					/>
+				)}
 				{couple.from && <Marker width={50} anchor={couple.from} />}
 				{couple.to && <Marker width={50} anchor={couple.to} />}
 			</Map>
