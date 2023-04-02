98c198e88469d2a6199d28cf2999d360b71339f3
Mael <laem@kont.me>
2023-04-02 15:25:33 +0200

    Meta pour l'accueil

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,12 @@
 import Link from 'next/link'
 import { Cards, LandingWrapper } from './UI'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title:
+		'Le classement des villes les plus cyclables et piétonnes - villes.plus',
+	description: `Une ville ne peut être agréable si elle est hostile aux piétons et aux vélos (ainsi qu'à toutes les mobilités légères). Nous évaluons, avec une méthodologie complètement transparente, une note de cyclabilité et de surface piétonne pour chaque ville et métropole française.`,
+}
 
 export default () => (
 	<LandingWrapper>
