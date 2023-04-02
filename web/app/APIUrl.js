0ca6c2c5d4cc29ed97bcddaa551bf57324a0f4af
Mael <laem@kont.me>
2023-04-02 15:14:51 +0200

    L'API est maintenant ailleurs

--- web/app/APIUrl.js
+++ web/app/APIUrl.js
@@ -1,4 +1,6 @@
 const url =
-	process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000/'
+	process.env.NODE_ENV === 'production'
+		? 'https://villes.plus/'
+		: 'http://localhost:3000/'
 
 export default url
