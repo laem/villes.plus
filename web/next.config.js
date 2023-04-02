036800c8c0cfc7f6c1bfeb845cc5ebcb8d7ebf0b
Mael <laem@kont.me>
2023-04-02 14:36:08 +0200

    On utilise eslint et typescript comme une aide, pas une contrainte

--- web/next.config.js
+++ web/next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
 	compiler: {
 		styledComponents: true,
 	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
 }
 
 module.exports = nextConfig
