2a7c06ecd2460d0059c91001e6cd97159612d522
Mael <laem@kont.me>
2023-04-05 20:17:52 +0200

    Exception pour recalculer avril

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ const readFile = async (dimension, ville, scope, res) => {
 		computeAndCacheCity(dimension, ville, scope, res, doNotCache)
 	if (doNotCache) return compute()
 	try {
+		throw Error("On doit recalculer les données d'avril")
 		const file = await s3
 			.getObject({
 				Bucket: BUCKET_NAME,
