c934dc9f07bd3e2fd687ec0ffc7d266945339578
Mael <laem@kont.me>
2023-04-28 19:00:02 +0200

    On étoffe la méthode cyclable. Photos réparées. Bouton plus visible

--- web/ArticleImportantLink.js
+++ web/ArticleImportantLink.js
@@ -6,16 +6,22 @@ export default ({ children, to }) => (
 		href={to}
 		css={`
 			text-decoration: none;
+			color: white;
+			font-weight: bold;
+			font-size: 150%;
+			text-align: center;
+			margin: 3rem 0;
+			display: block;
 		`}
 	>
 		<div
 			css={`
-				width: 16rem;
-				font-weight: bold;
+				width: 25rem;
+				max-width: 80%:
 				text-align: center;
 				max-width: 80vw;
 				background: var(--color1);
-				padding: 0.4rem 1rem;
+				padding: 1rem;
 				margin: 0 auto;
 			`}
 		>
