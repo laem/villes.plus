de11936b4bcc7c11cb11ac82e23d19394a0bfe7e
Mael <laem@kont.me>
2023-05-29 16:20:33 +0200

    Oubli du slash final

--- web/app/APIUrl.js
+++ web/app/APIUrl.js
@@ -1,6 +1,6 @@
 const url =
 	process.env.NODE_ENV === 'production'
-		? `https://villes-pr113.osc-fr1.scalingo.io`
+		? `https://villes-pr113.osc-fr1.scalingo.io/`
 		: `http://localhost:3000/`
 
 export default url
