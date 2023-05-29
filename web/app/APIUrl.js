7e722b0d5ae78be0707a1b4161b70bbea5626809
Mael <laem@kont.me>
2023-05-29 16:12:36 +0200

    Changement temporaire d'URL de sreveur prod pour juin

--- web/app/APIUrl.js
+++ web/app/APIUrl.js
@@ -1,6 +1,6 @@
 const url =
 	process.env.NODE_ENV === 'production'
-		? `https://api.villes.plus/`
+		? `https://villes-pr113.osc-fr1.scalingo.io`
 		: `http://localhost:3000/`
 
 export default url
