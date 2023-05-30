30c482111e720486c9e76af230bbc405ebc23887
Mael <laem@kont.me>
2023-05-30 20:36:44 +0200

    Oups, oublié de MAJ l'URL

--- web/app/APIUrl.js
+++ web/app/APIUrl.js
@@ -1,6 +1,6 @@
 const url =
 	process.env.NODE_ENV === 'production'
-		? `https://villes-pr113.osc-fr1.scalingo.io/`
+		? `https://api.villes.plus/`
 		: `http://localhost:3000/`
 
 export default url
