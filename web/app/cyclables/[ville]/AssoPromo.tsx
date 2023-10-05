4db7aae765906892da731d0379863d2e32728c81
Mael <laem@kont.me>
2023-10-05 23:41:52 +0200

    :bug: Qui empêchait l'affichage de l'asso des villes

--- web/app/cyclables/[ville]/AssoPromo.tsx
+++ web/app/cyclables/[ville]/AssoPromo.tsx
@@ -1,9 +1,10 @@
 import assos from '@/registre-assos-vélo.yaml'
 import Link from 'next/link'
 import Image from 'next/image'
+import { processName } from '@/../cyclingPointsRequests'
 export default ({ ville }) => {
 	const asso = assos.find(
-		(asso) => asso.ville === ville || asso.métropole === ville
+		(asso) => asso.ville === processName(ville) || asso.métropole === ville
 	)
 	if (!asso) return null
 
