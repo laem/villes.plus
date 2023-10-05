e30a76ab294f097645c9165c364cf9d6d9734cb2
Mael <laem@kont.me>
2023-10-06 00:02:03 +0200

    Affichage des assos sur plus de pages

--- web/app/cyclables/[ville]/AssoPromo.tsx
+++ web/app/cyclables/[ville]/AssoPromo.tsx
@@ -3,28 +3,34 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { processName } from '@/../cyclingPointsRequests'
 export default ({ ville }) => {
-	const asso = assos.find(
-		(asso) => asso.ville === processName(ville) || asso.métropole === ville
+	console.log('VILLE', ville)
+	const asso = assos.filter(
+		(asso) =>
+			asso.ville === processName(ville) ||
+			asso.métropole === ville ||
+			asso.département === ville
 	)
-	if (!asso) return null
+	if (!asso.length) return null
 
 	return (
 		<p>
 			😠 Pas content ? Soutenez{' '}
-			<Link href={asso.site}>
-				<img
-					src={asso.logo}
-					style={{
-						objectFit: 'cover',
-						width: '2rem',
-						verticalAlign: 'middle',
-						margin: '0 .4rem 0 .2rem',
-						height: '2rem',
-					}}
-					alt={'Le logo de ' + asso.name}
-				/>
-				{asso.nom}
-			</Link>
+			{asso.map((asso) => (
+				<Link href={asso.site} key={asso.nom}>
+					<img
+						src={asso.logo}
+						style={{
+							objectFit: 'cover',
+							width: '2rem',
+							verticalAlign: 'middle',
+							margin: '0 .4rem 0 .2rem',
+							height: '2rem',
+						}}
+						alt={'Le logo de ' + asso.name}
+					/>
+					{asso.nom}
+				</Link>
+			))}
 			.
 		</p>
 	)
