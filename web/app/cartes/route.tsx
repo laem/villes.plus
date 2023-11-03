0a8316a053fa53b41d88b301aa0312921d7b4858
Mael <laem@kont.me>
2023-11-03 15:20:33 +0100

    Nouveau fichier région; téléchargement et affichage moche des formes

--- web/app/cartes/route.tsx
+++ web/app/cartes/route.tsx
@@ -2,21 +2,47 @@ import { ImageResponse } from 'next/server'
 import { GeoJSON2SVG } from 'geojson2svg'
 import features from './features.json'
 import { html } from 'satori-html'
+import régions from '../../régions.yaml'
 const converter = new GeoJSON2SVG()
-
-const svgStrings = converter.convert(features)
 
 export const runtime = 'edge'
 
-const Component = html(`
+const getGeo = async () => {
+	const req = await Promise.all(
+		régions.map(({ osmId }) =>
+			fetch(
+				`http://polygons.openstreetmap.fr/get_geojson.py?id=${osmId}&params=0.020000-0.005000-0.005000`
+			).then((r) => r.json())
+		)
+	)
+	console.log('req', req)
 
-				<svg fill="black" viewBox="0 0 400 400" style="width: 100%; height: 100%">
+	return {
+		type: 'FeatureCollection',
+		features: req.map((geometry) => ({
+			type: 'Feature',
+			properties: {
+				stroke: '#555555',
+				'stroke-width': 2,
+				'stroke-opacity': 1,
+				fill: '#e01b24',
+				'fill-opacity': 0.5,
+			},
+			geometry: geometry,
+		})),
+	}
+}
+
+export async function GET() {
+	const geo = await getGeo()
+	const svgStrings = converter.convert(geo)
+	const Component = html(`
+
+				<svg fill="" viewBox="0 0 400 400" style="width: 100%; height: 100%">
 
 ${svgStrings}
 
 				</svg>`)
-console.log(svgStrings)
-export async function GET() {
 	return new ImageResponse(
 		(
 			<div
@@ -24,7 +50,7 @@ export async function GET() {
 					display: 'flex',
 					fontSize: 40,
 					color: 'black',
-					background: 'blue',
+					background: 'transparent',
 					width: '100%',
 					height: '100%',
 					textAlign: 'center',
