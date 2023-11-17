a0f1494997f5247cca2d015192c31ab2b4882f9f
Mael <laem@kont.me>
2023-11-17 15:42:13 +0100

    Réduction conséquente de l'en-tête via un deuxième details

--- web/app/cyclables/[ville]/Header.tsx
+++ web/app/cyclables/[ville]/Header.tsx
@@ -16,89 +16,82 @@ export default async ({ ville, data }) => {
 
 	return (
 		<Header>
+			<Logo color={'black'} text={processName(ville)} cyclable align="start" />
+			<ImageAndScoreWrapper>
+				<SmallImageWrapper>
+					<Image
+						src={wikidata.image}
+						style={{ objectFit: 'cover' }}
+						fill={true}
+						alt={`Une photo emblématique du territoire mesuré (${ville})`}
+					/>
+				</SmallImageWrapper>
+				<CityNumericResult {...{ cyclable: true, ville, initialData: data }} />
+			</ImageAndScoreWrapper>
+
 			<details>
 				<summary
 					style={css`
 						list-style-type: none;
+						font-size: 90%;
+						text-decoration: underline;
+						cursor: help;
 					`}
 				>
-					<Logo
-						color={'black'}
-						text={processName(ville)}
-						cyclable
-						align="start"
-					/>
-					<ImageAndScoreWrapper>
-						<SmallImageWrapper>
-							<Image
-								src={wikidata.image}
-								style={{ objectFit: 'cover' }}
-								fill={true}
-								alt={`Une photo emblématique du territoire mesuré (${ville})`}
-							/>
-						</SmallImageWrapper>
-						<CityNumericResult
-							{...{ cyclable: true, ville, initialData: data }}
-						/>
-					</ImageAndScoreWrapper>
-				</summary>
-
-				<div
-					style={css`
-						background: white;
-						padding: 1rem;
-					`}
-				>
-					<p style={{ marginBottom: 0 }}>
-						Voici le score et l'analyse de cyclabilité de {processName(ville)}.
-					</p>
-					<p>
-						Chaque mois, nous faisons rouler des 🚴 cyclistes virtuels sur une
-						multitude d'itinéraires, et nous comptons la proportion des
-						kilomètres{' '}
-						<details style={{ display: 'inline' }}>
-							<summary
-								style={{
-									cursor: 'help',
-									listStyleType: 'none',
-									display: 'inline',
-								}}
-							>
-								<strong
-									style={css`
-										border-bottom: 2px solid #8f68ea;
-									`}
-								>
-									vraiment sécurisés
-								</strong>
-								.
-							</summary>
-							<div style={{ borderLeft: '3px solid #8f68ea' }}>
-								<p>
-									Nous considérons comme sécurisées les voies cyclables séparées
-									des voitures et piétons ou des vélorues où le vélo est
-									prioritaire. Les pistes cyclables peintes au sol qui longent
-									des voitures garées ne sont pas sécurisées.
-								</p>
-								<p>
-									Les voies de bus où les cyclistes doivent cohabiter avec des
-									véhicules de 10 tonnes non plus. Les chemins piétons de balade
-									non plus.
-								</p>
-							</div>
-						</details>
-					</p>
-					<RésuméChiffré data={data} name={processName(ville)} />
-					<div
-						style={{
-							textAlign: 'right',
-						}}
+					<p
+						style={css`
+							margin-bottom: 0;
+							margin-left: 0;
+						`}
 					>
-						<small>
-							<Link href="/explications/cyclables">En savoir plus</Link> sur la
-							méthode d'évaluation.
-						</small>
-					</div>
+						Voici le score de cyclabilité de {processName(ville)}.
+					</p>
+				</summary>
+				<p>
+					Chaque mois, nous faisons rouler des 🚴 cyclistes virtuels sur une
+					multitude d'itinéraires, et nous comptons la proportion des kilomètres{' '}
+					<details style={{ display: 'inline' }}>
+						<summary
+							style={{
+								cursor: 'help',
+								listStyleType: 'none',
+								display: 'inline',
+							}}
+						>
+							<strong
+								style={css`
+									border-bottom: 2px solid #8f68ea;
+								`}
+							>
+								vraiment sécurisés
+							</strong>
+							.
+						</summary>
+						<div style={{ borderLeft: '3px solid #8f68ea' }}>
+							<p>
+								Nous considérons comme sécurisées les voies cyclables séparées
+								des voitures et piétons ou des vélorues où le vélo est
+								prioritaire. Les pistes cyclables peintes au sol qui longent des
+								voitures garées ne sont pas sécurisées.
+							</p>
+							<p>
+								Les voies de bus où les cyclistes doivent cohabiter avec des
+								véhicules de 10 tonnes non plus. Les chemins piétons de balade
+								non plus.
+							</p>
+						</div>
+					</details>
+				</p>
+				<RésuméChiffré data={data} name={processName(ville)} />
+				<div
+					style={{
+						textAlign: 'right',
+					}}
+				>
+					<small>
+						<Link href="/explications/cyclables">En savoir plus</Link> sur la
+						méthode d'évaluation.
+					</small>
 				</div>
 			</details>
 		</Header>
--- web/app/cyclables/[ville]/HeaderUI.tsx
+++ web/app/cyclables/[ville]/HeaderUI.tsx
@@ -2,5 +2,5 @@
 import styled from 'styled-components'
 
 export const Header = styled.header`
-	padding: 0 0 1rem 0;
+	padding: 0 0 0.6rem 0;
 `
