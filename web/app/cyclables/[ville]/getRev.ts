0e26472dd2b36c74bb484f394417ea311de59de4
Mael <laem@kont.me>
2023-11-17 16:17:03 +0100

    Couleurs des lignes de Lyon, suite à un changement dans les données
    https://github.com/benoitdemaegdt/voieslyonnaises/commit/efec70d3c834fdfb5d169c277bcaa895f3773191#r132852738

--- web/app/cyclables/[ville]/getRev.ts
+++ web/app/cyclables/[ville]/getRev.ts
@@ -1,6 +1,7 @@
 import { OverpassInstance, request } from '@/../cyclingPointsRequests'
 import { fetchRetry } from '@/../utils'
 import osmtogeojson from 'osmtogeojson'
+import lyonColors from '@/rev/Métropole de Lyon/couleurs.yaml'
 
 export default async (name) => {
 	if (name === 'Métropole de Lyon') {
@@ -15,18 +16,24 @@ export default async (name) => {
 				return res.json()
 			})
 		)
-		console.log(lines)
 
 		const json = {
 			type: 'FeatureCollection',
 			features: lines.map((line) => line.features).flat(),
 		}
-		return {
+		const result = {
 			...json,
-			features: json.features.filter(
-				({ geometry }) => geometry.type === 'LineString'
-			),
+			features: json.features
+				.filter(({ geometry }) => geometry.type === 'LineString')
+				.map((f) => ({
+					...f,
+					properties: {
+						...f.properties,
+						color: lyonColors['' + f.properties.line],
+					},
+				})),
 		}
+		return result
 	}
 	if (name === 'Rennes Métropole') {
 		const requestCore = `
